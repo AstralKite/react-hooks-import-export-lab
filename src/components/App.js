2d0bd7197210cdf1d68575e54fc6ddbfdecad6e7
Fausto Ramirez <ramirezmedia@gmail.com>
2023-02-09 21:26:43 -0500

    commit tree 72f7b0b7494062c7009ee3961c969336bd0e652f

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Home from "/home/faust/Development/code/phase-2/react-hooks-import-export-lab/src/components/Home.js";
+import NavBar from "/home/faust/Development/code/phase-2/react-hooks-import-export-lab/src/components/NavBar.js";
+import About from "/home/faust/Development/code/phase-2/react-hooks-import-export-lab/src/components/About.js";
 
 function App() {
   return (
